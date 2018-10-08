1fefeaff79286f2423a392d6eed06c33bad43022
xiandavis <xiandavis@yahoo.com>
2018-10-08 10:49:05 -0700

    segues

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -21,10 +21,9 @@ class ViewController: UIViewController {
         
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func unwindFromSkillVC(unwindSegueXian: UIStoryboardSegue) { // Mark named func this; could have been anything, just as func parameter name could be (added Xian to it)
+        
     }
 
 
