a965755c773ef6e4757050d3197ea68c4bc8f020
xiandavis <xiandavis@yahoo.com>
2018-10-09 13:45:17 -0700

    ibactions and data models

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,10 +10,14 @@ import UIKit
 
 class LeagueVC: UIViewController {
     
+    var player: Player! // implicitly unwrapped because we don't want code to run at all if there isn't a player
+    
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        player = Player() // initialize struct, when view loads it will create new player
     }
 
     @IBAction func onNextTapped(_ sender: Any) {
@@ -23,8 +27,20 @@ class LeagueVC: UIViewController {
 //            performSegue(withIdentifier: "womens", sender: self)
 //        }
     }
-
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens") // Mark just came up with these strings to refer back to
+    }
+    @IBAction func onWomensTapped(_ sender: Any) { // Mark moved logic code from here and put it inside func selectLeague() below
+        selectLeague(leagueType: "womens")
+    }
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
     
+    func selectLeague(leagueType: String) { // Mark just came up with this func name, param name
+        player.desiredLeague = leagueType // desiredLeague declared in Player.swift model file (opt-click var name above to see this)
+        nextBtn.isEnabled = true // allows segue to 3rd VC to operate
+    }
 
     /*
     // MARK: - Navigation
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Christian Davis on 10/9/18.
+//  Copyright © 2018 xianapps. All rights reserved.
+//
+
+import Foundation
+
+struct Player { // we access this from the LeagueVC.swift controller file
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
